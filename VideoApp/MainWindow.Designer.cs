2dd5ad73716d8a4dcfe0cbef17794e4939cfab0d
cotter3 <cotter3@ivytech.edu>
2019-06-08 23:15:19 -0400

    Add project files.

---
+++ VideoApp/MainWindow.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace VideoApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.countdown = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // countdown
+            // 
+            this.countdown.Location = new System.Drawing.Point(20, 20);
+            this.countdown.Name = "countdown";
+            this.countdown.Size = new System.Drawing.Size(100, 20);
+            this.countdown.TabIndex = 1;
+            this.countdown.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(526, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(152, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Visible = false;
+            // 
+            // axWindowsMediaPlayer
+            // 
+            this.axWindowsMediaPlayer.Enabled = true;
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(1560, 850);
+            this.axWindowsMediaPlayer.TabIndex = 0;
+            this.axWindowsMediaPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer_PlayStateChange);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.countdown);
+            this.Controls.Add(this.axWindowsMediaPlayer);
+            this.DoubleBuffered = true;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SOP Videos";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+        private System.Windows.Forms.TextBox countdown;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ VideoApp/MainWindow.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace VideoApp
+{
+
+    public partial class MainWindow : Form
+    {
+        public WMPPlayState playState { get; }
+        public int countTimer = 300;
+        public static string currentComp;
+        public int id;
+        public System.Boolean fullScreen { get; set; }
+
+        public Timer timer1 { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+              countTimer--;
+              countdown.Text = countTimer.ToString();
+            if (countTimer == 0)
+            {
+                dataGridView1.DataSource = null;
+                //starts sql connection
+                SqlConnection con = new SqlConnection(@"Server = Server;Initial Catalog = Catalog;Persist Security Info=True;User ID=User;Password=Password");
+                con.Open();
+
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select Something from Somewhere where This = That";
+                cmd.Parameters.AddWithValue("@WcData", System.Environment.MachineName);
+                // Setting command timeout to 60 second
+                cmd.CommandTimeout = 60;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException)
+                {
+                    Console.WriteLine("Got an expected SqlException due to command timeout on line 4259");
+                    Console.WriteLine(e);
+                }
+
+                //queries/fills table
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                object IdCode;
+                IdCode = dt.Compute("Sum(Something)", "");
+                if (string.IsNullOrEmpty(Convert.ToString(IdCode)))
+                {
+                    if (axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying && id == 0)
+                    {
+                        //rechecks to see if still in running with out restarting the video below
+                        countTimer = 500;
+                    }
+                    else
+                    {
+                        //when not running is in running or no video in sql query play demo video
+                        axWindowsMediaPlayer.URL = @"Somewhere";
+                        axWindowsMediaPlayer.Ctlcontrols.play();
+                        axWindowsMediaPlayer.settings.setMode("loop", true);
+                        countTimer = 500;
+                        id = 0;
+                    }
+                }
+                else if (id == Convert.ToInt32(IdCode))
+                {
+                    //resets the timer since the video didnt change instead of reloading the video
+                    countTimer = 500;
+                }
+                else
+                {
+                    //plays new video based on sql query from Database
+                    axWindowsMediaPlayer.URL = @"Somewhere".Replace("%IdCode%", Convert.ToString(IdCode) + ".wmv");
+                    axWindowsMediaPlayer.Ctlcontrols.play();
+                    axWindowsMediaPlayer.settings.setMode("loop", true);
+                    countTimer = 500;
+                    id = Convert.ToInt32(IdCode);
+                }
+            }
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            //loads all the settings and timer for the video player upon start of app
+            currentComp = System.Environment.MachineName;
+            textBox1.Text = currentComp;
+            axWindowsMediaPlayer.settings.autoStart = true;
+            axWindowsMediaPlayer.Ctlcontrols.play();
+            axWindowsMediaPlayer.settings.setMode("loop", true);
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+        }
+        private void axWindowsMediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            //sets the video player to fullscreen if playing
+            if (this.axWindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                {
+                    this.axWindowsMediaPlayer.fullScreen = true;
+                }
+        }
+    }
+}
